599dab35ffac94d3f57d9ecb5e30fecc0a051792
Tliimfee <Tliimfee@gmail.com>
2012-11-24 23:44:35 +0000

    added level constants

--- js/constants.js
+++ js/constants.js
@@ -13,5 +13,14 @@ var Consts =
         SPACE : 32,
         ENTER : 13,
         ESC : 27
+    },
+    tile_colours:
+    {
+        0xffffff:0,//White
+        0x000000:1,//Black
+        0xC3C3C3:3,//Gray
+        0xff0000:2,//Red
+        0x22b14c:5,//Green
+        0xb97a57:4,//Brown
     }
 }

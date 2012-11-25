4ba22867af2a9104c6518013f1a0c637a1254fd1
Tliimfee <Tliimfee@gmail.com>
2012-11-25 14:56:40 +0000

    loaded tile images

--- js/content.js
+++ js/content.js
@@ -7,10 +7,29 @@ function Content()
     this.load = function()
     {
         image["playerWalk"] = new Image();
-        image["playerWalk"].src = "img/playerWalk.png";
+        image["playerWalk"].src = "img/furniture/furniturePlayer.png";
         
         level["level01"] = new Image();
         level["level01"].src = "data/level01.png";
+        
+        
+        image["guard"] = new Image();
+        image["guard"].src = "img/furniture/furnitureGuard.png";
+        
+        image["boss"] = new Image();
+        image["boss"].src = "img/furniture/furnitureBoss.png";
+    
+        image["door"] = new Image();
+        image["door"].src = "img/furniture/furnitureDoor.png";
+        
+        image["floor"] = new Image();
+        image["floor"].src = "img/tiles/tileFloor.png";
+        
+        image["wall"] = new Image();
+        image["wall"].src = "img/tiles/tileWall.png";
+        
+    
+    
     }
     
     this.getLevel = function(lvl)

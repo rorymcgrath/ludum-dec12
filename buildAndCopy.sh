776d4bcc8e9e365b4ee7047e0c3c87dff847dcd4
Donagh Hatton <donagh@donagh-laptop.(none)>
2012-11-25 11:52:37 +0000

    build script updated

--- buildAndCopy.sh
+++ buildAndCopy.sh
@@ -3,3 +3,4 @@ scp index.html donagh@lifenoodles.com:~/www/ludum-n12/
 scp game.html donagh@lifenoodles.com:~/www/ludum-n12/
 scp game.js donagh@lifenoodles.com:~/www/ludum-n12/
 scp -r img donagh@lifenoodles.com:~/www/ludum-n12/
+scp -r data donagh@lifenoodles.com:~/www/ludum-n12/
